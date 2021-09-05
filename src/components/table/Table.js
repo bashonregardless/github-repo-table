59084a8256e728b5e19df7ed574c06db03a802d3
bashonregardless <theunforgiivensol@gmail.com>
2021-09-05 13:01:56 +0530

    chore(): Table.js Data fetching implementation. See Question(Q) in comment

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
 function Table(props) {
-  const [] = useState();
+  const [fetchStatus, fetchData] = useState(true);
 
   useEffect(() => {
 	async function fetchFromApi(url) {
 	  const response = await fetch(url);
 	  return response.json();
-	}
+	};
+
 	fetchFromApi("https://api.github.com/users/landstrider/repos")
 	.then(data => {
 	  console.log(data);
+	  fetchData(false);
 	})
 	.catch(err => {
 	  console.log("An error occured");
 	  console.log(err);
 	  // An UI displaying the error state is to be displayed
 	});
-  });
+  }, [fetchStatus]); // Q. Why is it that if fetchStatus is specified as dependency, then data is getting fetched twice?
 
+  if (fetchStatus) return <div>Fetching...</div>;
   return (
  	<table>
 	  <thead></thead>
