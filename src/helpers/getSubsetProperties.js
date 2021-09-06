668932ddfd3360ce2d341cbcffa3ab96126e5121
bashonregardless <theunforgiivensol@gmail.com>
2021-09-06 21:41:54 +0530

    chore(experimental code): Testing robustness of getSubsetProperties function against raw data(in file rawData.js in Table component)

--- src/helpers/getSubsetProperties.js
+++ src/helpers/getSubsetProperties.js
@@ -1,10 +1,12 @@
+import rawData from '../components/table/rawdata.js';
+
 function getSubsetProperties(data = {}, tableFields = [], obj = {}, basename = '') {
-  for (let key in data) {
-	if (Object.prototype.hasOwnProperty.call(data, key)) {
+  for (let key in rawData) {
+	if (Object.prototype.hasOwnProperty.call(rawData, key)) {
 	  if (tableFields.some(validKey => validKey === basename + key)) {
-		obj[basename + key] = data[key];
+		obj[basename + key] = rawData[key];
 	  }
-	  if (data[key] !== null && typeof(data[key]) == 'object') {
+	  if (rawData[key] !== null && typeof(rawData[key]) == 'object') {
 		// DFS of the object tree
 		let bname = basename + key + '_';
 		//basename =  basename + key + '_';
@@ -14,7 +16,7 @@ function getSubsetProperties(data = {}, tableFields = [], obj = {}, basename = '
 		// Then passing basename as parameter to getSubsetProperties function will keep a single 
 		// identity through out recursion.
 		// TODO Is there a better way to do this?
-		getSubsetProperties(data[key], tableFields, obj, bname);
+		getSubsetProperties(rawData[key], tableFields, obj, bname);
 	  }
 	}
   }
