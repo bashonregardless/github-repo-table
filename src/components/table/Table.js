c0042377e1b991f1d2c253573489c3d9c6787287
bashonregardless <theunforgiivensol@gmail.com>
2021-09-08 19:01:10 +0530

    chore(doubt): Data fetching logic improvement under progress. See Q in this commit

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -30,22 +30,33 @@ function Table(props = {}) {
 	  dispatch({ type: 'error', payload: {tableData, error} });
 	  fetchData(false); // TODO Try refetching data
 	});
-  }, [error]);
+  }, [tableData, error]);
   // TODO Q. Why is it that if isFetching is specified as dependency, then data is getting fetched twice?
   // instead of getting fetched once like it should when component mounts?
   // VERIFY THIS: It is because a state (isFetching, in this case) changes when the effect first runs
   // on first mount of the component, which again triggers a re-render, after the dom is painted, the
   // effect runs again, only this time the value of "isfetching" state does not change.
   //
-  // TODO Q. Why is it that when only error is specified as a dependency in the useEffect, and when 
-  // the network is disconnected, the program gets into a nasty infinite loop. Also, note that when
-  // when the network is connected the problem does not occur?
+  // TODO Q. Why is it that when only error state is specified as a dependency in the useEffect, and when 
+  // the network is disconnected, the program gets into a nasty infinite loop? Also, note that when
+  // when the network is connected the problem does not occur. The program successfullly fetches data.
   // VERIFY THIS: It is because the error action is getting set(changed) by dispatch in the case when 
   // network is disconnected. This triggers a re render and an attempt to fetch the data is made again
-  // which fails too, and the loop continues. TODO, here note that the new error getting set must be
+  // which fails too, and the loop continues. TODO, here note that the new error state getting set must be
   // different( have different references to error object) from the one set in previous fetch, so that 
   // a difference in state is calculated by react, which is the reason for re render.
-  // Whereas, it the error state does not change in the case when network is connected.
+  // Whereas, the error state does not change in the case when network is connected.
+  //
+  // TODO Q. Why is it that when only tableData state is specified as dependency in useEffect, and when 
+  // the network is connected, the program gets into a nasty inifinite loop? Also, note that when the 
+  // network is disconnected the problem doesn't occur. The program draws the error UI to the DOM.
+  // VERIFY THIS: It is because the tableData action is getting set(changed) by dispatch in the case when 
+  // network is disconnected. This triggers a re render and an attempt to fetch the data is made again
+  // which fails too, and the loop continues. TODO, here note that the new tableData state getting set must be
+  // different( have different references to tableData object) from the one set in previous fetch, so that 
+  // a difference in state is calculated by react, which is the reason for re render.
+  // Whereas, the tableData state does not change in the case when network is connected.
+  
   function drawHeader() {
 	 return ( 
 	   <tr>{
